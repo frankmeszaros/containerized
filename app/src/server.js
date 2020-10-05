30eb78cf8e28f19e5671aaa4a0e3c06c35acc886
frankmeszaros <fmeszaro@gmail.com>
2020-10-05 10:23:52 -0400

    initial push

--- app/src/server.js
+++ app/src/server.js
@@ -1,3 +1,5 @@
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+
 import App from './App';
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
@@ -6,46 +8,50 @@ import { renderToString } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
+const graphqlClient = new ApolloClient({
+	cache: new InMemoryCache(),
+	//   ssrMode: true,
+	//   link: new HttpLink({
+	//     uri:
+	//   })
+});
+
 const server = express();
 server
-  .disable('x-powered-by')
-  .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
-  .get('/*', (req, res) => {
-    const context = {};
-    const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
-    );
+	.disable('x-powered-by')
+	.use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+	.get('/*', (req, res) => {
+		const context = {};
+		const markup = renderToString(
+			<StaticRouter context={context} location={req.url}>
+				<App />
+			</StaticRouter>
+		);
 
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.status(200).send(
-        `<!doctype html>
+		if (context.url) {
+			res.redirect(context.url);
+		} else {
+			res.status(200).send(
+				`<!doctype html>
     <html lang="">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta charset="utf-8" />
         <title>Welcome to Razzle</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        ${assets.client.css ? `<link rel="stylesheet" href="${assets.client.css}">` : ''}
         ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
+			process.env.NODE_ENV === 'production'
+				? `<script src="${assets.client.js}" defer></script>`
+				: `<script src="${assets.client.js}" defer crossorigin></script>`
+		}
     </head>
     <body>
         <div id="root">${markup}</div>
     </body>
 </html>`
-      );
-    }
-  });
+			);
+		}
+	});
 
 export default server;
